ebabbe3ae6a238080475c182f79ecec0ebc57e71
Steveen <steveen.echeverri@gmail.com>
2020-06-21 19:20:37 -0500

    Spotify save Playlist and tracks

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -11,16 +11,9 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      searchResults: [{ name: 'Maroon 5', artist: 'Memories', album: 'Memories', id: 1 },
-      { name: 'Maroon 5', artist: 'Girls Like You', album: 'Red Pill Blues ', id: 2 },
-      { name: 'Maroon 5', artist: 'Sunday Morning', album: 'Songs About Jane', id: 3 },
-      { name: 'Maroon 5', artist: 'She Will Be Loved', album: 'Songs About Jane', id: 4 },
-      { name: 'Maroon 5', artist: 'Moves Like Jagger', album: 'Hands All Over', id: 5 },
-      { name: 'Maroon 5', artist: 'One More Night', album: 'One More Night', id: 6 }],
+      searchResults: [],
       playlistName: 'My Playlist',
-      playlistTracks: [{ name: 'The Scientist', artist: 'Coldplay', album: 'A Rush of Blood to the Head', id: 7 },
-      { name: 'Harder to Breathe', artist: 'Maroon 5', album: 'Songs About Jane', id: 8 },
-      { name: 'Pyramid Song', artist: 'Radiohead', album: 'Amnesiac', id: 9 }]
+      playlistTracks: []
     };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
@@ -53,6 +46,12 @@ class App extends React.Component {
   savePlaylist() {
     // alert('this method is button correctly!')
     const trackUris = this.state.playlistTracks.map(track => track.uri);
+    Spotify.savePlaylist(this.state.playlistName, trackUris).then(() => {
+      this.setState({
+        playlistName: 'New Playlist',
+        playlistTracks: []
+      })
+    })
   }
 
   search(term) {
